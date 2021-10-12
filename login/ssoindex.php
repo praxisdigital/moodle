bee34e875910a3b16ee2930cc06141fbd1e9ab6c
Ulrik McArdle <umc@praxis.dk>
2021-10-12 14:24:32 +0200

    MOODLE-5668 - Added try/catch in login/ssoindex.php and errors in language strings

--- login/ssoindex.php
+++ login/ssoindex.php
@@ -1,4 +1,17 @@
 <?php
-$bundledata =  @$_GET['bundleddata'];
-header('Location: ../auth/ef_webbog_sso/ssoindex.php?bundleddata='.$bundledata);
-exit;
+
+require_once __DIR__.'/../config.php';
+
+try{
+    $bundleddata = required_param('bundleddata', PARAM_RAW);
+    header('Location: /../auth/ef_webbog_sso/ssoindex.php?bundleddata='.$bundleddata);
+    die();
+}
+catch(\Exception $e){
+    if(debugging('', DEBUG_DEVELOPER)){
+        debugging($e->getMessage(), DEBUG_DEVELOPER, debug_backtrace());
+    }
+    else{
+        die(get_string('quiz_not_available', 'auth_ef_webbog_sso'));
+    }
+}
