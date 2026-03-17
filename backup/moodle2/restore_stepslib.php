b4d219da155dc12ebd00773cb265e3214f40bdf1
Halfdan Karlsen Monie <hkm@moxis.dk>
2026-03-17 14:53:17 +0100

    MDL-87893 Subsection Backup: Fixed empty sections being added on restore

--- backup/moodle2/restore_stepslib.php
+++ backup/moodle2/restore_stepslib.php
@@ -1660,6 +1660,10 @@ class restore_section_structure_step extends restore_structure_step {
                             $data, true);
                 }
             }
+            $target = $this->task->get_target();
+            if ($target === \backup::TARGET_CURRENT_ADDING || $target === \backup::TARGET_EXISTING_ADDING) {
+                $section->section = $this->get_last_section_number($this->get_courseid()) + 1;
+            }
 
             // Delegated sections should be always after the normal sections.
             $this->displace_delegated_sections_after($section->section);
